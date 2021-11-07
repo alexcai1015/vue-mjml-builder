56ba11dc99ae384b62896144a42099ec544f6ed2
sweb2 <ctalent5340@gmail.com>
2021-11-07 13:06:06 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app dark>
+    <router-view/>
+  </v-app>
+</template>
+
+<script>
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+}
+</script>
